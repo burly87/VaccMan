9fba17723f8680878df6acf75adc31265609f4cc
Tobias <tobias.forve@gmx.de>
2020-03-03 17:04:15 +0100

    Added movement and Shooting in MouseDir

---
+++ VaccManMobile/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rigidbody;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        // Vector2 lookDir = mousePos - rigidbody.position; // get vector in lookDir
+        // float angle = Mathf.Atan2(lookDir.y, lookDir.x);
+    }
+}
---
+++ VaccManMobile/Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bulletPref;
+    public float bulletForce = 20f;
+
+    [SerializeField]
+    private Transform aimTransform;
+    Vector3 mousePos;
+
+    void Update()
+    {
+       
+        Aim();
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPref, aimTransform.position, aimTransform.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(aimTransform.right * bulletForce,ForceMode2D.Impulse);
+    }
+
+    // Get Mouseposition, calculate direction vector and rotate the aimTransformObject in that direction
+    void Aim()
+    {
+        //get mousPos and rotate aimTransform
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 lookDir = (mousePos - this.transform.position).normalized;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0,0,angle);
+    }
+
+    // void OnCollisionEnter2D(Collider2D other)
+    // {
+    //     if(other.CompareTag("Player"))
+    //     {
+    //         return;
+    //     }
+    //     else
+    //     {
+    //         //apply dmg to object
+
+    //         //play particle effect
+
+    //         //destroy this
+    //         //Destroy(this);
+    //         Debug.Log("Hit " + other.name);
+    //     }
+    // }
+}
