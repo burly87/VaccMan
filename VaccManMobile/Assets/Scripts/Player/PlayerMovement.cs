e1ec73fa948e3dd6d34c51870dd8c0e736fa481d
Tobias <tobias.forve@gmx.de>
2020-03-08 18:09:02 +0100

    Blinking and Idle done

--- VaccManMobile/Assets/Scripts/Player/PlayerMovement.cs
+++ VaccManMobile/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rigidbody;
     Vector2 movement;
+    bool m_FacingRight = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +22,26 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        
+        if(movement.x > 0)
+        {
+            Flip();
+        }
     }
 
     void FixedUpdate()
     {
         rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
-
     }
+
+    private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 }
